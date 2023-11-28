d7760689b61b4077fe15b2e8ad6b0600d929d612
Grace Little <gracevlittle@gmail.com>
2023-11-28 09:53:46 -0600

    nav bar

--- components/NoAuthNavBar.js
+++ components/NoAuthNavBar.js
@@ -7,31 +7,31 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState(0); // Start with the index of the tab you want to be active initially
 
-  const handleChange = (newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
     // Perform actions based on the new value if needed
   };
 
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={(event, newValue) => handleChange(newValue)}>
+    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
       <BottomNavigationAction
         label="Recents"
-        value="recents"
+        value={0}
         icon={<RestoreIcon />}
       />
       <BottomNavigationAction
         label="Favorites"
-        value="favorites"
+        value={1}
         icon={<FavoriteIcon />}
       />
       <BottomNavigationAction
         label="Nearby"
-        value="nearby"
+        value={2}
         icon={<LocationOnIcon />}
       />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+      <BottomNavigationAction label="Folder" value={3} icon={<FolderIcon />} />
     </BottomNavigation>
   );
 }
